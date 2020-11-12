c2bb97aeeb6c2064312b07bd6be4d5776d48236f
Artur Tagisow <atagisov@protonmail.com>
2020-11-12 21:03:44 +0100

    Add loader

---
+++ src/components/VkLoader.vue
@@ -0,0 +1,79 @@
+<template>
+  <!-- eslint-disable-next-line vue/no-multiple-template-root -->
+  <template v-if="loading">
+    <div class="wrapper">
+      <div class="spinner spinner1" />
+      <div class="spinner spinner2" />
+    </div>
+  </template>
+  <!-- eslint-disable-next-line vue/no-multiple-template-root -->
+  <slot v-else />
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    loading: { type: Boolean, default: true },
+    size: {
+      type: String,
+      default: "100",
+      validator: (val: unknown) => !isNaN(Number(val)) && Number(val) > 0,
+    },
+  },
+  setup(props) {
+    return {
+      sizeFormatted: computed(() => `${Number(props.size)}px`),
+    };
+  },
+});
+</script>
+
+<!--eslint-disable-next-line prettier/prettier-->
+<style scoped lang="scss" vars={sizeFormatted}>
+@mixin anim-infinite($name, $duration) {
+  animation-name: $name;
+  animation-duration: $duration;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
+}
+.wrapper {
+  position: relative;
+}
+.spinner {
+  width: var(--sizeFormatted);
+  height: var(--sizeFormatted);
+  margin: 20px;
+  background: $color-primary;
+  position: absolute;
+}
+
+.spinner1 {
+  background: $color-primary;
+  @include anim-infinite(spin2, 1500ms);
+}
+.spinner2 {
+  background: $color-secondary;
+  border: $color-light 5px solid;
+  @include anim-infinite(spin2, 700ms);
+}
+
+@keyframes spin1 {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes spin2 {
+  from {
+    transform: rotate(90deg);
+  }
+  to {
+    transform: rotate(450deg);
+  }
+}
+</style>
