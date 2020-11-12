3f297a4bdbc432ad828f98dfbd730a4613b674a5
Artur Tagisow <atagisov@protonmail.com>
2020-11-12 20:08:40 +0100

    emplyees table

---
+++ src/modules/projects/molecules/EmployeesTable.vue
@@ -0,0 +1,22 @@
+<template>
+  <article>
+    <div v-for="employee in employees">
+      <div>{{ employee.firstName }} {{ employee.lastName }}</div>
+      <div v-text="employee.workTitle" />
+    </div>
+  </article>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import { Employee } from "@/mock-server";
+
+export default defineComponent({
+  props: {
+    employees: Array as PropType<Employee[]>,
+  },
+  setup(props) {},
+});
+</script>
+
+<style scoped lang="scss"></style>
